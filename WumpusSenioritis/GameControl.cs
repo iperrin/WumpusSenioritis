da88075bed544bb82ee0c8ec3de73857ae15e2ef
iperrin <36754433+iperrin@users.noreply.github.com>
2018-03-16 03:28:23 -0700

    Add files via upload

---
+++ WumpusSenioritis/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class GameControl
+{
+    //declare map object
+    //declare score object
+    //declare player object
+
+    public GameControl(int difficulty)
+	{
+        //call constructors for all respective classes
+	}
+
+    public void RunGame()
+    {
+
+    }
+
+    private Boolean Trivia(int num)
+    {
+        int count = 0;
+        for(int i = 0; i<num; i++)
+        {
+            if (/*run trivia*/) count++;
+        }
+        if (count > (num / 2)) return true;
+        return false;
+    }
+}
