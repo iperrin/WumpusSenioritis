a7117701e02fa10771dfa787f65281619d68184a
Unknown <isaac.j.perrin@gmail.com>
2018-03-23 10:01:34 -0700

    made it work kinda

--- Archive/3-20-2018/WumpusSenioritis/GameControl.cs
+++ Archive/3-20-2018/WumpusSenioritis/GameControl.cs
@@ -2,21 +2,30 @@
 
 public class GameControl
 {
-    Player player;
-    Map map;
-    TriviaManagement trivia;
+    //declare map object
+    //declare score object
+    //declare player object
+    //declare wumpus object
 
     public GameControl(int difficulty)
 	{
-        player = new Player();
-        map = new Map(difficulty);
-        trivia = new TriviaManagement();
-
+        //call constructors for all respective classes
 	}
 
     public void RunGame()
     {
 
+    }
+
+    private Boolean Trivia(int num)
+    {
+        int count = 0;
+        for(int i = 0; i<num; i++)
+        {
+            if (/*trivia.run trivia()*/) count++;
+        }
+        if (count > (num / 2)) return true;
+        return false;
     }
 
     private void updatePlayer(int newPosition)
---
+++ TEMP/WumpusGame/WumpusGame/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace WumpusTest
+{
+    class Player{
+        int arrows = 3;
+        int goldCoins;
+        int playerTurns;
+        int endingScore;
+        int currentRoom;
+
+        public void PlayerMove(int position){
+            //Moves the player to a certain position.
+            //Method is called by the Game Control Object (?) when there is a command for you to move the player.
+        }
+
+        //increase arrow count by 2
+        public void PurchaseArrow()
+        {
+            arrows += 2;
+        }
+
+        public void ShootArrow(int targetRoom){
+            arrows--;
+            if (GameControl.checkHit(targetRoom)){
+                GameControl.killWumpus();
+                GameControl.endScreen();
+            }
+            else{
+                if (arrows <= 0){
+                    GameControl.endScreen();
+                }
+                GameControl.moveWumpus();
+            }
+            //Haven't joined code yet so i just assumed some names for class and method names from cave.
+        }
+
+        public int PlayerGold(){
+            //Tracks the amount of gold that the player currently has.
+            return 0;
+        }
+
+        public String PlayerInventory(){
+            //Prints out the Player's inventory.
+            //Calls the Graphical Interface.
+            return PlayerArrows() + " " + PlayerGold() + " ";
+        }
+
+        public int NumberOfTurns(){
+            //Tracks the number of turns that the player currently has gone through.
+            return 0;
+        }
+
+        public int FinalScore(){
+            //Tracks the gold, turns, and arrows and figures out the final score.
+            return 0;
+        }
+    }
+}
