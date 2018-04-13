6a3fc5909c618f61999b2baead1eb8f3dd31626b
Unknown <isaac.j.perrin@gmail.com>
2018-04-12 20:22:55 -0700

    updated game contreol

--- Master/WumpusGame/WumpusGame/ProgramManager.cs
+++ Master/WumpusGame/WumpusGame/ProgramManager.cs
@@ -3,13 +3,29 @@
 public class ProgramManager
 {
 
-    graphicInterface GUI;
-    HighScore highScore;
-    GameControl currentGame;
+    private GameControl currentGame;
+    private struct info
+    {
+        public string name;
+        public int score;
+        public int turns;
+        public int gold;
+        public int arrows;
+        public int cave;
+    }
 
 	public ProgramManager()
 	{
-        //intiailize GUI views and such
+        
 	}
+
+    public void runGame(int difficulty)
+    {
+        currentGame = new GameControl(difficulty);
+        //HighScore.addScore();
+
+        ProgramManager.info.cave = 5;
+
+    }
 
 }
