0ac68663a3a0de809b811abcf39e7e3ed69c2421
macqt <37648456+macqt@users.noreply.github.com>
2018-04-20 21:37:32 -0700

    Update HighScore.cs

--- Master/WumpusGame/WumpusGame/HighScore.cs
+++ Master/WumpusGame/WumpusGame/HighScore.cs
@@ -1,34 +1,69 @@
-﻿using System;
+using System;
 using System.Collections;
+using Microsoft.VisualBasic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 
-class HighScore
+namespace Wumpus_Test
 {
-
-    public struct PlayerInfo
+    class HighScore
     {
-        public string name;
-        public int turns;
-        public int gold;
-        public int arrows;
-        public int cave;
+        //score turns gold arrows cave player
+       
+        private String[]  Record;
+        private String[] results;
+        public HighScore(String[] data)
+        {
+            results = data;
+            Record = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\record.txt");
+        }
+     
+        public void addScore()
+        {
+            int spot = -1;
+            for (int x = Record.Length-6, y = 0; y < 10;  x-=6, y++)
+            {
+                if(Int32.Parse(Record[x]) < Int32.Parse(results[0]))
+                {
+                    spot = x;  
+                }
+             }
+            if (spot != -1)
+            {
+                ReplaceScore(spot);
+            }
+        }
+        public String[] GetHighScores()
+        {
+            string[] scores = System.IO.File.ReadAllLines(@"C:\Users\Public\TestFolder\record.txt");
+            return scores;
+        }
+        public void ReplaceScore(int spot)
+        {
+            String[] newRecord = new String[Record.Length];
+            for (int x = 0; x < spot; x++)
+            {
+                newRecord[x] = Record[x];
+            }
+            newRecord[ spot ] = results[0];
+            newRecord[spot+1] = results[1];
+            newRecord[spot+2] = results[2];
+            newRecord[spot+3] = results[3];
+            newRecord[spot+4] = results[4];
+            newRecord[spot+5] = results[5];
+            for (int x = spot+6, y = spot; x < newRecord.Length; x++, y++)
+            {
+                newRecord[x] = Record[y];
+            }
+            Record = newRecord;
+        }
+        public void WriteNewScores()
+        {
+            System.IO.File.WriteAllLines(@"C:\Users\Public\TestFolder\record.txt", Record);
+        }
     }
 
-    private static String FileLocation = "tempLocation";
 
-    public ArrayList GetHighScore()
-    {
-        return Record;
     }
-    public void ReplaceScore()
-    {
-        //checks if score is good enough;
-    }
-    public void WriteNewScores(String StoredFiles)
-    {
-        //write to file eventually
-    }
-}
