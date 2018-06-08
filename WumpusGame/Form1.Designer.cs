7819c484b8d1cfc136045b4ef46edeb46ed94fba
nhulk <37612487+nhulk@users.noreply.github.com>
2018-06-07 18:02:58 -0700

    Cave TestForm
    Hm it isn't letting me rename these

---
+++ WumpusGame/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WumpusTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConstructorButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ConstructorButton
+            // 
+            this.ConstructorButton.AccessibleName = "ConstructorButton";
+            this.ConstructorButton.Location = new System.Drawing.Point(146, 87);
+            this.ConstructorButton.Name = "ConstructorButton";
+            this.ConstructorButton.Size = new System.Drawing.Size(108, 42);
+            this.ConstructorButton.TabIndex = 0;
+            this.ConstructorButton.Text = "ConstructorButton";
+            this.ConstructorButton.UseVisualStyleBackColor = true;
+            this.ConstructorButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 297);
+            this.Controls.Add(this.ConstructorButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConstructorButton;
+    }
+}
+
---
+++ WumpusGame/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WumpusTest
+{
+    public partial class Form1 : Form
+    {
+        private Cave _Cave;
+        private int randomCave;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            randomCave = r.Next(0, 5);
+            _Cave = new Cave();
+            _Cave.load(randomCave);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _Cave.getDoors();
+        }  
+    
+    }
+}
